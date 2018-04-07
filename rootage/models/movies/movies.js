a7b7f1d7f205edfa53bce3b32b6cc0645ca825cb
karthik <karthikg1643@gmail.com>
2018-04-07 15:26:53 +0530

    added type field to movies db schema

--- rootage/models/movies/movies.js
+++ rootage/models/movies/movies.js
@@ -6,8 +6,8 @@ const Schema = mongoose.Schema;
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('movies', new Schema({
     'id': { type : String , unique : true, required : true, dropDups: true },
-    'title': String,
-    'year': String,
+    'title': { type : String , required : true },
+    'year': { type : String , required : true },
     'genres': [String],
     'ratings': [Number],
     'poster': String,
@@ -19,5 +19,6 @@ module.exports = mongoose.model('movies', new Schema({
     'storyline': String,
     'actors': [String],
     'imdbRating': Number,
-    'posterurl': String
+    'posterurl': String,
+    'type': String
 }));
